9c09a1c8483f322ba0ca07a09174d1d9cb002169
moneem79 <moneemmonower79@gmail.com>
2025-06-16 23:28:02 +0600

    HTTP GET method

--- app/src/main/java/com/example/useretrofit/MainActivity.kt
+++ app/src/main/java/com/example/useretrofit/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.useretrofit.models.PostResponseItem
 import com.example.useretrofit.ui.theme.USeRetrofitTheme
 
@@ -37,8 +45,31 @@ class MainActivity : ComponentActivity() {
                 }
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
+                    LazyColumn (modifier = Modifier.fillMaxSize()
+                        .padding(innerPadding)
+                        .fillMaxSize()){
+                        items (posts){
+                            Card(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp),
+                                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+                            ) {
+                                Column(modifier = Modifier.padding(16.dp)) {
+                                 Text(it.userId.toString())
+                                 Text(it.title.toString())
+                                 Text(it.body.toString())
+
+
+                                }
+                            }
+                        }
+
+                        }
+                    }
+
                 }
             }
         }
     }
-}
+
